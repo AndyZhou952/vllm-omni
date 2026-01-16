3f696b92ef5a878f3dc62273d6e6bfd5c9660cde
AndyZhou952 <jzhoubc@connect.ust.hk>
2026-01-16 17:15:01 +0800

    LoRAConfig in init
    Signed-off-by: AndyZhou952 <jzhoubc@connect.ust.hk>

--- vllm_omni/config/__init__.py
+++ vllm_omni/config/__init__.py
@@ -3,7 +3,9 @@ Configuration module for vLLM-Omni.
 """
 
 from vllm_omni.config.model import OmniModelConfig
+from vllm_omni.config.lora import LoRAConfig
 
 __all__ = [
     "OmniModelConfig",
+    "LoRAConfig",
 ]
