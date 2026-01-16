3c776faf5f9d1d70ccb42ab1c4728da00b35194a
AndyZhou952 <jzhoubc@connect.ust.hk>
2026-01-16 15:53:48 +0800

    diffusion self-defined layers

--- vllm_omni/diffusion/lora/layers/column_parallel_linear.py
+++ vllm_omni/diffusion/lora/layers/column_parallel_linear.py
@@ -6,6 +6,8 @@ from __future__ import annotations
 from vllm.lora.layers.column_parallel_linear import (
     ColumnParallelLinearWithLoRA,
     MergedColumnParallelLinearWithLoRA,
+    MergedQKVParallelLinearWithLoRA,
+    QKVParallelLinearWithLoRA,
 )
 
 from .base_linear import DiffusionBaseLinearLayerWithLoRA
@@ -29,6 +31,30 @@ class DiffusionMergedColumnParallelLinearWithLoRA(
 ):
     """
     Diffusion MergedColumnParallelLinear (gate_up_proj) with LoRA.
+    Prioritize apply() in DiffusionBaseLinearLayerWithLoRA
+    """
+
+    pass
+
+
+class DiffusionQKVParallelLinearWithLoRA(
+    DiffusionBaseLinearLayerWithLoRA,
+    QKVParallelLinearWithLoRA,
+):
+    """
+    Diffusion QKVParallelLinear with single LoRA.
+    Prioritize apply() in DiffusionBaseLinearLayerWithLoRA
+    """
+
+    pass
+
+
+class DiffusionMergedQKVParallelLinearWithLoRA(
+    DiffusionBaseLinearLayerWithLoRA,
+    MergedQKVParallelLinearWithLoRA,
+):
+    """
+    Diffusion MergedQKVParallelLinear (to_qkv) with 3 LoRAs.
     Prioritize apply() in DiffusionBaseLinearLayerWithLoRA
     """
 
