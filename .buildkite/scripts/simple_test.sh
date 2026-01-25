e883f9720373e68f09d47f7aaea1593eaf93c0c9
dongbo910220 <1275604947@qq.com>
2026-01-26 02:02:14 +0800

    tests: reorganize diffusion LoRA unit tests
    Signed-off-by: dongbo910220 <1275604947@qq.com>

--- .buildkite/scripts/simple_test.sh
+++ .buildkite/scripts/simple_test.sh
@@ -52,7 +52,6 @@ VENV_PYTHON="${VENV_DIR}/bin/python"
 
 "${VENV_PYTHON}" -m pytest -v -s tests/entrypoints/
 "${VENV_PYTHON}" -m pytest -v -s tests/diffusion/cache/
-"${VENV_PYTHON}" -m pytest -v -s tests/diffusion/lora/test_lora_manager.py
-"${VENV_PYTHON}" -m pytest -v -s tests/diffusion/lora/test_lora_utils.py
+"${VENV_PYTHON}" -m pytest -v -s tests/diffusion/lora/
 "${VENV_PYTHON}" -m pytest -v -s tests/model_executor/models/qwen2_5_omni/test_audio_length.py
 "${VENV_PYTHON}" -m pytest -v -s tests/worker/
