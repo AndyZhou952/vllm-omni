e6fdf39254e875046979b62672295a4b51b8e4c8
AndyZhou952 <jzhoubc@connect.ust.hk>
2026-08-19 16:38:49 +0800

    [Bugfix][Diffusion] Preserve stage attention backend
    Signed-off-by: AndyZhou952 <jzhoubc@connect.ust.hk>

--- tests/engine/test_async_omni_engine_stage_init.py
+++ tests/engine/test_async_omni_engine_stage_init.py
@@ -1336,6 +1336,38 @@ def test_resolve_stage_configs_preserves_stage_diffusion_attention(monkeypatch):
     assert stage_configs[0].engine_args.diffusion_attention_config is existing_attention
 
 
+def test_resolve_stage_configs_preserves_stage_diffusion_attention_backend(monkeypatch):
+    import vllm_omni.engine.async_omni_engine as engine_mod
+
+    engine = object.__new__(AsyncOmniEngine)
+    stage_cfg = types.SimpleNamespace(
+        stage_type="diffusion",
+        engine_args=types.SimpleNamespace(
+            diffusion_attention_backend="TORCH_SDPA",
+            diffusion_attention_config=None,
+            lora_path=None,
+            lora_scale=None,
+            enable_sleep_mode=None,
+            quantization_config=None,
+        ),
+    )
+
+    monkeypatch.setattr(
+        engine_mod,
+        "load_and_resolve_stage_configs",
+        lambda *args, **kwargs: ("dummy-config", [stage_cfg], None),
+    )
+
+    _config_path, stage_configs = engine._resolve_stage_configs(
+        model="dummy-model",
+        kwargs={"diffusion_attention_backend": "FLASH_ATTN"},
+        trust_remote_code=False,
+    )
+
+    assert stage_configs[0].engine_args.diffusion_attention_backend == "TORCH_SDPA"
+    assert stage_configs[0].engine_args.diffusion_attention_config is None
+
+
 def test_resolve_stage_configs_does_not_inject_diffusion_attention_into_llm_stage(monkeypatch):
     import vllm_omni.engine.async_omni_engine as engine_mod
 
